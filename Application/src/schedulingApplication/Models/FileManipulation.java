96b0eea3d7f23197677bf7ea3a7ecbdfcf3b835e
ZacharyDavidSaunders <zacharydavidsaunders@gmail.com>
2018-05-26 10:34:05 -0700

    Fixed Log Filename

--- Application/src/schedulingApplication/Models/FileManipulation.java
+++ Application/src/schedulingApplication/Models/FileManipulation.java
@@ -5,20 +5,20 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class FileManipulation {
-    
-    private static String logFileName = "/C195log.txt";
+
+    private static String logFileName = "/SchedulingApplicationLog.txt";
     private static String logFilePath = "";
     private static File file;
-    
+
     public static void writeToFile(String newData) {
-        
+
         getUsersHomeDesktop();
-        
+
         try {
             File file = new File(logFilePath.concat(logFileName));
             if(file.exists()){
                 newData = getFileContents(logFilePath+logFileName).concat(newData);
-            }    
+            }
             FileWriter fw = new FileWriter(logFilePath.concat(logFileName));
             BufferedWriter br = new BufferedWriter(fw);
             br.write(newData);
@@ -26,21 +26,21 @@ public class FileManipulation {
         } catch (IOException e) {
             System.out.println("ERROR Writing to logfile: "+e);
         }
-        
+
     }
-    
+
     public static void getUsersHomeDesktop(){
         String desktop = System.getProperty("user.home") + "/Desktop";
         desktop.replace("\\", "/");
         logFilePath = desktop;
     }
-    
+
     public static String getFileContents(String filePath) throws IOException{
         String contents = "";
         contents = new String ( Files.readAllBytes(Paths.get(filePath)) );
         return contents;
     }
-    
+
     public static String getFullFilePath(){
         return logFilePath.concat(logFileName);
     }
